3684a1af49a46cf81b2ba5ab4912aa5947564e1b
ruley-git <66989186+ruley-git@users.noreply.github.com>
2022-06-21 13:01:51 +1000

    Reduced light level to display sun image

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 let Light = 0
 basic.forever(function () {
     Light = pins.analogReadPin(AnalogPin.P0)
-    if (Light > 200) {
+    if (Light > 100) {
         basic.showLeds(`
             # . # . #
             . # # # .
