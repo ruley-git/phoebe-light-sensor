8dd69a60243d6034278b248170df4f8733dde664
ruley-git <66989186+ruley-git@users.noreply.github.com>
2022-06-21 12:57:24 +1000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,21 @@
+let Light = 0
+basic.forever(function () {
+    Light = pins.analogReadPin(AnalogPin.P0)
+    if (Light > 200) {
+        basic.showLeds(`
+            # . # . #
+            . # # # .
+            # # # # #
+            . # # # .
+            # . # . #
+            `)
+    } else {
+        basic.showLeds(`
+            # # # . .
+            . # # # .
+            . . # # .
+            . # # # .
+            # # # . .
+            `)
+    }
+})
